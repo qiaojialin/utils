6e562d28c430b615b0d6c5c39af77a9e6e3c39b1
qiaojialingithub <646274302@qq.com>
2018-05-30 08:26:05 +0800

    add wechat paper

--- src/main/java/io/ByteArrayOutputStreamTest.java
+++ src/main/java/io/ByteArrayOutputStreamTest.java
@@ -1,5 +1,6 @@
 package io;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
@@ -10,34 +11,51 @@ import java.io.IOException;
 public class ByteArrayOutputStreamTest {
 
     public static void main(String[] args) throws IOException {
+
+        outputStreamTest();
+        inputStreamTest();
+    }
+
+    public static void outputStreamTest() throws IOException {
         byte[] b1 = new byte[]{0, 1, 2};
         byte[] b2 = new byte[]{3};
-        byte[] b3 = new byte[]{4, 5, 6, 7};
 
         ByteArrayOutputStream s1 = new ByteArrayOutputStream();
         ByteArrayOutputStream s2 = new ByteArrayOutputStream();
-        ByteArrayOutputStream s3 = new ByteArrayOutputStream();
-        ByteArrayOutputStream total = new ByteArrayOutputStream();
 
         s1.write(b1);
-        s2.write(b2);
-        s3.write(b3);
+        s1.write(b2);
 
-        System.out.println(s1.size());
-        System.out.println(s2.size());
-        System.out.println(s3.size());
+        s1.writeTo(s2);
 
-        //将 s1，s2，s3 的数据写入 total 流
-        s1.writeTo(total);
-        s2.writeTo(total);
-        s3.writeTo(total);
-
-        s2.write(b1);
-
-        byte[] ret = total.toByteArray();
-        for (int i = 0; i < ret.length; i++) {
-            System.out.print(ret[i]);
-        }
-
+        byte[] ret = s2.toByteArray();
+        print(ret);
     }
+
+
+    public static void inputStreamTest() throws IOException {
+        byte[] b1 = new byte[]{1,2,3,4};
+        ByteArrayInputStream s1 = new ByteArrayInputStream(b1);
+
+        System.out.println("s1 available: "+s1.available());
+        byte[] b2 = new byte[2];
+        s1.read(b2);
+        print(b2);
+
+
+        System.out.println("s1 available: " + s1.available());
+        s1.read(b2);
+        print(b2);
+
+        System.out.println("s1 available: " + s1.available());
+    }
+
+    public static void print(byte[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]);
+        }
+        System.out.println();
+    }
+
+
 }
--- src/main/java/io/RandomAccessFileTest.java
+++ src/main/java/io/RandomAccessFileTest.java
@@ -3,46 +3,40 @@ package io;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-
-
 public class RandomAccessFileTest {
     private static String fileName = "src/main/resources/test.myfile";
     private static File file = new File(fileName);
-
-
     public static void main(String[] args) throws IOException {
-        if (file.exists())
-            file.delete();
-
         byte[] a = new byte[10];
-
         RandomAccessFile randomAccess = new RandomAccessFile(file, "rw");
+        //跳过 2 个字节
         randomAccess.seek(2);
-        //写入6个bytes
+        //写入 6 个字节
         randomAccess.write(new byte[]{1,2,3,4,5,6});
-        System.out.println("position: " + randomAccess.getFilePointer());
-
-        //移动文件头到 offset=3 的位置，offset相对于文件头部
-        randomAccess.seek(3);
-        System.out.println("position: " + randomAccess.getFilePointer());
-
-        //从 offset=3 处写入一个byte，直接覆盖原来的数据
-        randomAccess.write(new byte[]{7});
-
-        System.out.println("file length: "+randomAccess.length());
-
+        System.out.println("当前位置: " + randomAccess.getFilePointer());
+        System.out.println("文件长度: "+randomAccess.length());
 
         //从头读取 10 个 bytes
         randomAccess.seek(0);
         randomAccess.read(a);
         print(a);
 
-        //seek 到 offset=2 处读取 10 个 bytes
-        randomAccess.seek(2);
-        a = new byte[10];
+        //移动到 offset=3 的位置
+        randomAccess.seek(3);
+        System.out.println("当前位置: " + randomAccess.getFilePointer());
+        //写入一个byte，直接覆盖原来的数据
+        randomAccess.write(new byte[]{7});
 
-        //读 2 个 byte，写入到 a 的第 1 个字节处
-        randomAccess.read(a, 1, 2);
+        //从头读取 10 个 bytes
+        randomAccess.seek(0);
+        randomAccess.read(a);
+        print(a);
+
+        //seek 到 offset=2 处
+        randomAccess.seek(2);
+        a = new byte[3];
+        //读 2 个字节到 a
+        randomAccess.read(a, 0, 2);
         print(a);
 
         randomAccess.close();
@@ -50,11 +44,11 @@ public class RandomAccessFileTest {
     }
 
 
-    public static void print(byte[] a) {
-        for(int i = 0; i < a.length; i++) {
-            System.out.print(a[i]);
+    private static void print(byte[] array) {
+        System.out.print("读取：");
+        for (byte b : array) {
+            System.out.print(b);
         }
         System.out.println();
     }
-
 }
